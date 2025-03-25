0bd4a56774a14f5e8b828ced4339a257b9f0c8e1
vixtruong <vipro20161@gmail.com>
2025-03-26 01:47:48 +0700

    chore: update auth interceptor logic

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   // const token = localStorage.getItem('access_token');
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0cnVvbmdsZWR1Y3ZpMTAwMUBnbWFpbC5jb20iLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3NDI5MjY2MTMsImV4cCI6MTc0MjkyNzUxMywiaWF0IjoxNzQyOTI2NjEzLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo1MDA0IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwNCJ9.8uOciI6zGcDcCdl_5jdaXp-5yamKn_892Sio7GMcxSQ";
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0cnVvbmdsZWR1Y3ZpMTAwMUBnbWFpbC5jb20iLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE3NDI5MjgwNjEsImV4cCI6MTc0MjkyODk2MSwiaWF0IjoxNzQyOTI4MDYxLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo1MDA0IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwNCJ9.K6HVtHiRqp17cWyVsr9xtuwn2sFwv0IxwwhN9wPXl0I";
 
   if (token) {
     const clonedRequest = req.clone({
