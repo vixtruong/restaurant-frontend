34ebc2b97c9957a55ab12e3e8253ccc9da4eafe9
vixtruong <vipro20161@gmail.com>
2025-07-29 10:29:56 +0700

    release: ver 2.0.0

--- src/app/features/admin/pages/table-detail/table-detail.component.ts
+++ src/app/features/admin/pages/table-detail/table-detail.component.ts
@@ -1,20 +1,27 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { OrderService } from '../../../../core/services/order.service';
 import { OrderDetailDto } from '../../../../core/dtos/order-detail.dto';
 
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-table-detail',
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, ButtonModule, ToastModule, ConfirmDialogModule],
+  providers: [MessageService, ConfirmationService],
   templateUrl: './table-detail.component.html',
   styleUrl: './table-detail.component.css'
 })
 export class TableDetailComponent {
   orderService = inject(OrderService);
   route = inject(ActivatedRoute);
+  router = inject(Router);
+  confirmationService = inject(ConfirmationService);
 
   tableNumber?: number;
   orderDetail?: OrderDetailDto;
@@ -34,5 +41,23 @@ export class TableDetailComponent {
       error: err => console.log(err),
       complete: () => console.log('Load order detail complete.')
     });
+  }
+
+  confirmCreateBill() {
+    this.confirmationService.confirm({
+      message: `Are you sure to create transaction for ${this.orderDetail?.customerName}?`,
+      header: 'Confirm create transaction',
+      icon: 'pi pi-question-circle',
+      acceptLabel: 'Create',
+      rejectLabel: 'Cancle',
+      acceptButtonStyleClass: 'p-button-info',
+      accept: () => {
+        this.createBill(this.orderDetail?.orderId!);
+      }
+    });
+  }
+
+  createBill(orderId: Number) {
+    this.router.navigate(['/admin/invoice', orderId]);
   }
 }
